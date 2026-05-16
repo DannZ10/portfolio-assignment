5bb96147f36c945256c8d1f73323cb3864792234
DannZ10 <hamdan.ysf26@gmail.com>
2026-05-16 20:02:01 +0700

    init: setup basic portfolio structure, gitignore, and dark mode feature

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+const { theme } = require("../../tailwind.config");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getEelementById('theme-toggle');
+    const htmlElement = document.documentElement;
+
+    const applySavedTheme = () => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme === 'dark') {
+            htmlElement.classList.add('dark');
+        } else if (savedTheme === 'light') {
+            htmlElement.classList.remove('dark');
+        } else {
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                htmlElement.classList.add('dark');
+            }
+        }          
+    };
+
+    applySavedTheme();
+
+    themeToggleBtn.addEventListener('click', () => {
+        htmlElement.classList.toggle('dark');
+
+        if (htmlElement.classList.contains('dark')) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    });
+});
